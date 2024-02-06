2b35a2ea237d7ea1fa6b01627f5d0db7a95e37d2
saikiran0701 <62210706+reddysai616@users.noreply.github.com>
2024-02-06 23:28:17 +0530

    base

---
+++ components/index.js
@@ -0,0 +1,37 @@
+import Header from "./Header";
+import Card from "./Card";
+import Footer from "./Footer";
+import HeroSection from "./HeroSection";
+import Selector from "./Selector";
+import SingleCard from "./SingleCard";
+import SwapComponent from "./SwapComponent";
+import SwapField from "./SwapField";
+import Table from "./Table";
+import TokenBalance from "./TokenBalance";
+import TransactionStatus from "./TransactionStatus";
+
+// svg
+import Facebook from "./SVG/Facebook";
+import Insta from "./SVG/Insta";
+import Logo from "./SVG/Logo";
+import Menu from "./SVG/Menu";
+import Twitter from "./SVG/Twitter";
+
+export {
+  Header,
+  Card,
+  Footer,
+  HeroSection,
+  Selector,
+  SingleCard,
+  SwapComponent,
+  SwapField,
+  Table,
+  TokenBalance,
+  TransactionStatus,
+  Facebook,
+  Insta,
+  Logo,
+  Menu,
+  Twitter,
+};
--- deploy/contracts/CustomEx.sol
+++ deploy/contracts/CustomEx.sol
@@ -1,3 +1,46 @@
 // SPDX-License-Identifier: GPL-3.0
 
 pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract CustomeToken is ERC20 {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        _mint(msg.sender, 100000 * 10 ** 18);
+    }
+}
+
+contract CustomDex {
+    string[] public tokens =["usd" ,"matic","eth",""]
+    mapping(string => ERC20) public tokenInstanceMap;
+
+    unit256 ethval = 10000000;
+
+    struct History{
+        unit256 historyId;
+        string tokenA;
+        string tokenB;
+        unit256 inputValue;
+        unit256 outputValue;
+        address userAddress;
+    }
+    unit256 public _historyIndex;
+    mapping(unit256 => history) private historys;
+
+    constructor(){
+        for(unit i=0; i<tokens.length; i++){
+            CustomeToken token = new CustomeToken(tokens[i] , tokens[i]);
+            tokenInstanceMap[tokens[i]]=tokens;
+
+        }
+    }
+    function  getBalance(string memory tokenNAme , address _address) public view returns (unit256){
+        return tokenInstanceMap[tokenNAme].balanceOf(_address);
+    }
+
+    function getTotalSupply(string memory tokenName) public view returns (unit256){
+        return tokenInstanceMap[tokenName].totalSupply();
+
+    }
+
+
+}
