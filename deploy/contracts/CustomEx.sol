ff3fc6c4993217c76e3081d3adf03b73b03ff527
saikiran0701 <62210706+reddysai616@users.noreply.github.com>
2024-02-09 17:40:45 +0530

    sol

--- deploy/contracts/CustomEx.sol
+++ deploy/contracts/CustomEx.sol
@@ -33,14 +33,39 @@ contract CustomDex {
 
         }
     }
-    function  getBalance(string memory tokenNAme , address _address) public view returns (unit256){
-        return tokenInstanceMap[tokenNAme].balanceOf(_address);
+    function  getBalance(string memory tokenName , address _address) public view returns (unit256){
+        return tokenInstanceMap[tokenName].balanceOf(_address);
     }
 
     function getTotalSupply(string memory tokenName) public view returns (unit256){
         return tokenInstanceMap[tokenName].totalSupply();
 
     }
+
+    function getName(string memory tokenName) public view returns (string memory){
+        return tokenInstanceMap[tokenName].name();
+    }
+
+    function getTokenAddress(string memory tokenName) public view returns (address){
+        return address(tokenInstanceMap[tokenName]);
+    }
+
+    function getBalance() public view returns (uint256){
+        return address(this).balance;
+    }
+
+function _transactionHistory(string memory tokenName , string memory etherToken , unit256 inputValue, unit256 outputValue) internal {
+    _historyIndex++;
+    unit256 _historyId = _historyIndex;
+    History storage history = historys[_historyId];
+    history.userAddress = msg.sender;
+    history.tokenA = tokenName;
+    history.tokenB = etherToken;
+    history.inputValue = inputValue;
+    history.outputValue = outputValue;
+
+}
+
 
 
 }
